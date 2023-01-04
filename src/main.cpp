1ccbad69aa93c3df9c4898fe5a9376aea40665c2
Matijs Behrens Z230 <matijs.behrens@outlook.com>
2023-01-04 15:56:35 +0100

    major update, added dhcp support, mqtt remote config

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,35 @@
 #include <TelnetStream.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <PubSubClient.h>
+
 
 #include "Credentials.h"
 #include "Ota.h"
 #include "Controls.h"
 #include "Sensors.h"
 
+// function declarations
+// void mqtt_callback(char* topic, byte* message, unsigned int length);
+void transmit_mqtt(const char * Field, const char * payload);
+
 char ssid[20];
 char password[20];
 
-IPAddress local_IP(192, 168, 4, 1);
-IPAddress gateway(192, 168, 4, 254);
-IPAddress subnet(255, 255, 255, 0);
+// IPAddress local_IP(192, 168, 4, 1);
+// IPAddress gateway(192, 168, 4, 254);
+// IPAddress subnet(255, 255, 255, 0);
 
 ESP8266WebServer server(80);
+
+// MQTT stuff
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char* applicationUUID = "123456789";
+const char* default_mqtt_server = "192.168.2.201";
+const char* TOPIC = "Climate/Floor";
+// MQTT stuff end
 
 #define SENSOR_CV_TEMPERATURE T2
 #define SENSOR_VLOER_TEMPERATURE T1
@@ -46,17 +61,29 @@ struct settings
   char ssid[32];         // the SSID of the netwerk
   char password[32];     // the WifiPassword
   char energy_topic[32]; // the path to the topic on which the current wattage is found.
+  char mqtt_server[32];  // Addres of the MQTT server
+  float setPoint;
+  float CvOvertemp;
+  float hysteresis;
 } user_wifi = {};
 
 unsigned long startMillis; // some global variables available anywhere in the program
 unsigned long currentMillis;
+unsigned long pumpOnMillis; // timestamp when the pump is on.
+unsigned long pumpPostRunTime = 900000;  // 900sec / 15min number of milliseconds the pump needs to run after turned off.
+unsigned long ac24Millis;
+unsigned long ac24PostRunTime = 20000; // keep trafo on for 20sec.
 unsigned long period = 1000; // the value is a number of milliseconds
 
-const byte Led_pin = D4; // using the built in LED
-const byte Pump = D3;
-const byte Heater_1 = D2; // using the built in LED
-const byte Heater_3 = D6; // using the built in LED
-const byte Heater_2 = D5; // using the built in LED
+float default_setPoint = 20.0f;
+float default_CvOvertemp = 27.0f;
+float default_hysteresis = 0.25f;
+
+const byte Led_pin   = D4; // using the built in LED
+const byte Pump      = D3;
+const byte Heater    = D2; // using the built in LED
+const byte ac24      = D6; // using the built in LED
+const byte Valve     = D5; // using the built in LED
 
 const byte iotResetPin = D1;
 
@@ -65,33 +92,86 @@ void ApMode()
 {
   WiFi.mode(WIFI_AP);
   Serial.print("Setting soft-AP configuration ... ");
-  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+  //Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
 
   Serial.print("Setting soft-AP ... ");
-  Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
+  Serial.println(WiFi.softAP(ssid, NULL) ? "Ready" : "Failed!");
   // WiFi.softAP(ssid);
   // WiFi.softAP(ssid, password, channel, hidden, max_connection)
 
   Serial.print("Soft-AP IP address = ");
   Serial.println(WiFi.softAPIP());
-  // WiFi.softAP("Sensor", "admin");
+  
   configuration.state = wifi_ap_mode;
 }
 
+void mqtt_callback(char* topic, byte* message, unsigned int length) {
+  // Serial.print("Message arrived on topic: ");
+  // Serial.print(topic);
+  // Serial.print(". Message: ");
+  String messageTemp;
+  
+  // char mytopic[32];
+  // snprintf(mytopic, 32, "sensor-%08X/set", ESP.getChipId());
+
+  for (unsigned int i = 0; i < length; i++) {
+    // Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  // Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  
+    
+  if (messageTemp.startsWith("floorsetpoint=")) {
+    user_wifi.setPoint = messageTemp.substring(14).toFloat();
+    Serial.printf("floor Setpoint: %f\n", user_wifi.setPoint);
+  } else if (messageTemp.startsWith("gcvsetpoint=")) {
+    user_wifi.CvOvertemp = messageTemp.substring(12).toFloat();
+    Serial.printf("gcv Setpoint: %f\n", user_wifi.CvOvertemp);
+  } else if (messageTemp.startsWith("hysteresis=")) {
+    user_wifi.hysteresis = messageTemp.substring(11).toFloat();
+    Serial.printf("hysteresis: %f\n", user_wifi.hysteresis);
+  } else if (messageTemp == "settings=read" ) {
+    EEPROM.get(0, user_wifi);
+  } else if (messageTemp == "settings=write" ) {
+       EEPROM.put(0, user_wifi);
+    EEPROM.commit();
+  } else if (messageTemp == "settings=default" ) {
+    user_wifi.setPoint = default_setPoint;
+    user_wifi.CvOvertemp = default_CvOvertemp;
+    user_wifi.hysteresis = default_hysteresis;
+    strcpy(user_wifi.mqtt_server, default_mqtt_server);
+    EEPROM.put(0, user_wifi);
+    EEPROM.commit();
+  } else if (messageTemp == "system=restart" ) {
+    ESP.restart();
+  } else if (messageTemp == "system=showip" ) {
+    char ip[32];
+    snprintf(ip, 32, "%d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3] );
+    transmit_mqtt("ip",ip);
+  }
+
+
+
+}
 
 void setup()
 {
-  pinMode(Heater_1, OUTPUT);
-  pinMode(Heater_2, OUTPUT);
-  pinMode(Heater_3, OUTPUT);
+  pinMode(Heater, OUTPUT);
+  pinMode(ac24, OUTPUT);
+  pinMode(Valve, OUTPUT);
   pinMode(Pump, OUTPUT);
   pinMode(iotResetPin, INPUT_PULLUP);
 
-  // First turn every thing off
-  TurnOff(Heater_1);
-  TurnOff(Heater_2);
-  TurnOff(Heater_3);
-  TurnOff(Pump);
+  // First enable safe mode
+  TurnOff(Heater);
+  TurnOn(ac24);
+  TurnOn(Valve);
+  TurnOn(Pump);
 
   Serial.begin(9600);
   Serial.println("Booting");
@@ -116,7 +196,13 @@ void setup()
     configuration.state = wifi_connecting;
     // Read network data.
     EEPROM.get(0, user_wifi);
+
+    Serial.printf("Wifi ssid: %s\n", user_wifi.ssid);
+    Serial.printf("MQTT host: %s\n", user_wifi.mqtt_server);
+
+
     WiFi.mode(WIFI_STA);
+    WiFi.setAutoReconnect(true); // werkt dit?
     WiFi.begin(user_wifi.ssid, user_wifi.password);
     byte tries = 0;
     while (WiFi.status() != WL_CONNECTED)
@@ -162,7 +248,8 @@ void setup()
 
   case wifi_ap_mode:
     TurnOn(Led_pin);
-    Serial.println("started WiFi AP");
+    Serial.printf("started WiFi AP %s\n", ssid);
+
     server.on("/", handlePortal);
     server.begin();
     break;
@@ -172,12 +259,72 @@ void setup()
     Serial.println("Ready for OTA");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+    // set up mqtt stuff
+    client.setServer(user_wifi.mqtt_server, 1883);
+    client.setCallback(mqtt_callback);
+    char ip[32];
+    snprintf(ip, 32, "%d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3] );
+    transmit_mqtt("ip",ip);
     break;
 
   default:
     break;
   }
 }
+
+
+
+int connect_mqtt() {
+  if (client.connected()) {
+    return 1;
+  }
+
+  Serial.print("Attempting MQTT connection... ");
+
+  // // Create a random client ID
+  // String clientId = "ESP8266Client-";
+  // sprintf(ssid, "sensor-%08X\n", ESP.getChipId());
+  // clientId += String(random(0xffff), HEX);
+
+  // Attempt to connect
+  if (client.connect(ssid)) {
+    Serial.println("connected");
+    char topic[32];
+    snprintf(topic, 32, "sensor-%08X/set", ESP.getChipId());
+    Serial.println(topic);
+    client.subscribe(topic);
+    return 1;
+  }
+
+  Serial.print("failed, rc=");
+  Serial.print(client.state());
+  return 0;
+}
+
+void transmit_mqtt(const char * Field, const char * payload) {
+  if (connect_mqtt()) {
+    char topic[75];
+    snprintf(topic, 75, "sensor-%08X/%s/%s", ESP.getChipId(), TOPIC,Field);
+    // Serial.println(topic);
+    client.publish(topic, payload);
+  } else {
+    Serial.println("MQTT ISSUE!!");
+  }
+}
+
+void transmit_mqtt_influx(const char * Field, float value) {
+  char payload[75];
+  snprintf(payload, 75, "climatic,host=sensor-%08X %s=%f",ESP.getChipId(),Field, value);
+  transmit_mqtt("state", payload);
+}
+
+void transmit_mqtt_float(const char * Field, float value) {
+  char payload[20];
+  snprintf(payload, 20, "%f",value);
+  transmit_mqtt(Field, payload);
+}
+
+
 
 void ShowClients()
 {
@@ -215,11 +362,14 @@ void ShowClients()
 
 
 byte loper = 0x01;
-float setPoint = 20.0f;
-float CvOvertemp = 27.0f;
-float hysteresis = 0.25f;
-float VloerSetPoint = setPoint;
-int power = 1;
+
+float VloerSetPoint = user_wifi.setPoint;
+int ECV_Power = 1;
+int GCV_State = 0;
+int Pump_Power = 1;
+int Valve_State = 1;
+int Valve_Previous_state = 0; 
+int ac24_Power = 1;
 
 void loop()
 {
@@ -228,77 +378,126 @@ void loop()
   case wifi_ready:
     // wifi connected to network. ready
     ArduinoOTA.handle();
-    period = 1000; // blink slow
     break;
   case wifi_ap_mode:
     // could not connect, waiting for new configuration.
     server.handleClient();
-
-    period = 900; // slow blink
     break;
 
   default:
     break;
   }
 
+ 
+  client.loop(); // mqtt loop
   // Blinking led.
 
   currentMillis = millis();                  // get the current "time" (actually the number of milliseconds since the program started)
-  if (currentMillis - startMillis >= period) // test whether the period has elapsed
+  if (((currentMillis - startMillis) >= period) && (configuration.state == wifi_ready) )// test whether the period has elapsed
   {
-    sensor_loop();
+    //sensor_loop(); // show all data from all found sensors. 
+    sensors.requestTemperatures();
 
-    float vloerTemp = sensors.getTempC(SENSOR_VLOER_TEMPERATURE );
+    float vloerTemp = sensors.getTempC(SENSOR_VLOER_TEMPERATURE ) + 0.31f; // slight offset compensation.
+    float cvAanvoerTemp = sensors.getTempC(SENSOR_CV_TEMPERATURE);
+
+    transmit_mqtt_influx("Wifi.rssi",WiFi.RSSI());
+    transmit_mqtt_influx("VloerTemp.c",vloerTemp);
+    transmit_mqtt_influx("AanvoerTemp.c",cvAanvoerTemp);
+    transmit_mqtt_influx("VloerSetTemp.c",user_wifi.setPoint);
+    transmit_mqtt_influx("AanvoerSetTemp.c",user_wifi.CvOvertemp);
+    transmit_mqtt_influx("Hysteresis.c",user_wifi.hysteresis);
+
+
     if ( vloerTemp >= VloerSetPoint ) {
-      VloerSetPoint = setPoint - hysteresis;
-      power = 0; 
+      VloerSetPoint = user_wifi.setPoint - user_wifi.hysteresis;
+      ECV_Power = 0; 
       // turn off  
     } else if ( vloerTemp <= VloerSetPoint ) {
-      VloerSetPoint = setPoint + hysteresis;
-      power = 1;
+      VloerSetPoint = user_wifi.setPoint + user_wifi.hysteresis;
+      ECV_Power = 1;
       // turn on
     }
     
-    if ( sensors.getTempC(SENSOR_CV_TEMPERATURE) > CvOvertemp ) {
+    // If water from the Gas CV is warmer that CvOvertemp assume the Room Thermostat has kicked in.
+    if ( cvAanvoerTemp > user_wifi.CvOvertemp ) {
       Serial.println("CV heating detected TURN OFF");
-      power = 0;
-      TurnOn(Pump);
+      GCV_State = 1;
+      transmit_mqtt_influx("Aanvoer.state",1.0f);
+      Valve_State = 1;
     } else {
-      TurnOff(Pump);
+      GCV_State = 0;
+      transmit_mqtt_influx("Aanvoer.state",0.0f);
     }
 
-    if ( power == 1) {
-      TurnOn(Heater_1);
-      TurnOn(Pump);
+    
+
+    // only when ECV is needed and no Gas CV is active
+    if ( (ECV_Power == 1) && (GCV_State == 0) ) {
+      TurnOn(Heater);
+      transmit_mqtt_influx("Heater.state",1.0f);
+      Valve_State = 0;
     } else {
-      TurnOff(Heater_1);
-      TurnOff(Pump);
+      TurnOff(Heater);
+      transmit_mqtt_influx("Heater.state",0.0f);
     }
 
-    // if ((loper & 0x02) == 0x02)
-    //   TurnOn(Heater_1);
-    // else
-    //   TurnOff(Heater_1);
-    // if ((loper & 0x04) == 0x04)
-    //   TurnOn(Heater_2);
-    // else
-    //   TurnOff(Heater_2);
-    // if ((loper & 0x08) == 0x08){
-    //   TurnOn(Heater_3);
-    //   loper = 0;
-    // }
-    // else
-    //   TurnOff(Heater_3);
-    // if ((loper & 0x01) == 0x01)
-    //   TurnOn(Pump);
-    // else
-    //   TurnOff(Pump);
+    // if ECV or GCV is active turn on the pump.
+    if ( (ECV_Power == 1 ) || (GCV_State == 1)) {
+      Pump_Power = 1;
+    } else {
+      Pump_Power = 0;
+    }
 
-    // if (loper == 4)
-    //   sensor_loop();
 
-    // loper++;
 
+    if ( Pump_Power == 1 ) {
+      pumpOnMillis = currentMillis;
+      TurnOn(Pump);
+      transmit_mqtt_influx("Pomp.state",1.0f);
+    } else {
+      if ( (currentMillis - pumpOnMillis )  >= pumpPostRunTime ) {
+        TurnOff(Pump);
+        transmit_mqtt_influx("Pomp.state",0.0f);
+      } else {
+        transmit_mqtt_influx("Pomp.state",0.5f);
+        Serial.println("Pump off delayed.");
+      }
+    }
+    
+    if ( Valve_State == 1) {
+      TurnOn(Valve);
+      transmit_mqtt_influx("Valve.state",1.0f);
+    } else if ( Valve_State == 0) {
+      TurnOff(Valve);
+      transmit_mqtt_influx("Valve.state",0.0f);
+    } 
+
+    if ( Valve_State != Valve_Previous_state ) {
+      Valve_Previous_state = Valve_State;
+      ac24_Power = 1;
+    }
+
+    if ( ac24_Power == 1) {
+      ac24_Power = 0;
+      ac24Millis = currentMillis;
+      TurnOn(ac24);
+      transmit_mqtt_influx("ac24.state",1.0f);
+    } else {
+      if ( (currentMillis - ac24Millis )  >= ac24PostRunTime ) {
+        TurnOff(ac24);
+        transmit_mqtt_influx("ac24.state",0.0f);
+      } else {
+        TurnOn(ac24);
+        transmit_mqtt_influx("ac24.state",1.0f);
+      }
+    }
+    
+    
+  
+    
+
+ 
     // digitalWrite(Heater_1, !digitalRead(Heater_1));  //if so, change the state of the LED.  Uses a neat trick to change the state
     if (configuration.state == wifi_ap_mode)
       ShowClients();
@@ -316,6 +515,13 @@ void handlePortal()
     strncpy(user_wifi.ssid, server.arg("ssid").c_str(), sizeof(user_wifi.ssid));
     strncpy(user_wifi.password, server.arg("password").c_str(), sizeof(user_wifi.password));
     user_wifi.ssid[server.arg("ssid").length()] = user_wifi.password[server.arg("password").length()] = '\0';
+
+    // load operational defaults
+    user_wifi.setPoint = default_setPoint;
+    user_wifi.CvOvertemp = default_CvOvertemp;
+    user_wifi.hysteresis = default_hysteresis;
+    strcpy(user_wifi.mqtt_server, default_mqtt_server);
+   
     EEPROM.put(0, user_wifi);
     EEPROM.commit();
 
